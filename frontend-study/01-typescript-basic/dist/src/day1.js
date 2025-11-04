dfd9cbacc92938ec58d66beacd21892cc9f1f364
Jonggu-code <00whdcks@gmail.com>
2025-11-04 15:20:13 +0900

    1104 TypeScript Study

---
+++ frontend-study/01-typescript-basic/dist/src/day1.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const multiply = (x, y) => {
+    return x * y;
+};
+console.log(multiply(3, 9));
+//# sourceMappingURL=day1.js.map
